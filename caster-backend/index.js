bed21ae7e78d31c1ffe5768c6da43dc7fdab4dc3
SimonArcher <simonnline@gmail.com>
2023-06-05 23:45:23 +0200

    Added backend

---
+++ caster-backend/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/generate-chat', async (req, res) => {
+    try {
+        const { celebrity1, celebrity2, topic } = req.body;
+
+        // Structure the chat models and message prompt
+        const messages = [
+            { role: 'system', content: `You are a helpful conversation script writer assistant` },
+            { role: 'user', content: `Write a conversation between ${celebrity1} and ${celebrity2} discussing ${topic}` },
+        ];
+
+        // Send the request to OpenAI's API
+        const completion = await openai.createCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: messages,
+        });
+
+        // Extract the chat text from the response
+        const chatText = completion.choices[0].message.content;
+
+        // Send the chat text back to the client
+        res.send({ chatText });
+    } catch (error) {
+        if (error.response) {
+            console.error(error);
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
